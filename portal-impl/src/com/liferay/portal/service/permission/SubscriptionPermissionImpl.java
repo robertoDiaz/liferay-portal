c5cb35bad022116cfcbfe58c301f81737f0feb38
Sergio Gonzalez <sergio.gonzalez@liferay.com>
2014-03-12 17:09:33 +0100

    LPS-44593 Pass the object to avoid one extra query

--- portal-impl/src/com/liferay/portal/service/permission/SubscriptionPermissionImpl.java
+++ portal-impl/src/com/liferay/portal/service/permission/SubscriptionPermissionImpl.java
@@ -240,7 +240,7 @@ public class SubscriptionPermissionImpl implements SubscriptionPermission {
 				JournalArticleLocalServiceUtil.getArticle(classPK);
 
 			return JournalArticlePermission.contains(
-				permissionChecker, article.getResourcePrimKey(), actionId);
+				permissionChecker, article, actionId);
 		}
 		else if (className.equals(JournalFolder.class.getName())) {
 			Group group = GroupLocalServiceUtil.fetchGroup(classPK);
@@ -288,7 +288,7 @@ public class SubscriptionPermissionImpl implements SubscriptionPermission {
 			WikiPage page = WikiPageLocalServiceUtil.getPageByPageId(classPK);
 
 			return WikiPagePermission.contains(
-				permissionChecker, page.getResourcePrimKey(), actionId);
+				permissionChecker, page, actionId);
 		}
 
 		return null;
