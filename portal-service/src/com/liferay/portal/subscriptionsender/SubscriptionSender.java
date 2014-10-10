cda3f6c100d85ae8743c7bd7c1bb855f816e0056
Roberto Díaz <roberto.diaz@liferay.com>
2014-10-10 17:00:21 +0200

    Expose the minimum amount of methods as possible (in further changes more methods will be included as needed as permission checking ones but not by the moment)

--- portal-service/src/com/liferay/portal/subscriptionsender/SubscriptionSender.java
+++ portal-service/src/com/liferay/portal/subscriptionsender/SubscriptionSender.java
@@ -14,15 +14,9 @@
 
 package com.liferay.portal.subscriptionsender;
 
-import com.liferay.portal.kernel.mail.SMTPAccount;
-import com.liferay.portal.kernel.util.EscapableObject;
-import com.liferay.portal.service.ServiceContext;
+import com.liferay.alloy.tools.model.BaseModel;
 
-import java.io.File;
 import java.io.Serializable;
-
-import java.util.Locale;
-import java.util.Map;
 
 /**
  * @author Roberto Díaz
@@ -30,82 +24,8 @@ import java.util.Map;
 
 public interface SubscriptionSender extends Serializable {
 
-	public void addFileAttachment(File file);
-
-	public void addFileAttachment(File file, String fileName);
-
-	public void addPersistedSubscribers(String className, long classPK);
-
-	public void addRuntimeSubscribers(String toAddress, String toName);
-
-	public void flushNotifications() throws Exception;
-
-	public void flushNotificationsAsync();
-
 	public String getClassName();
 
-	public Object getContextAttribute(String key);
-
-	public String getMailId();
-
-	public void initialize() throws Exception;
-
-	public void setBody(String body);
-
-	public void setBulk(boolean bulk);
-
-	public void setClassName(String className);
-
-	public void setClassPK(long classPK);
-
-	public void setCompanyId(long companyId);
-
-	public void setContextAttribute(String key, EscapableObject<String> value);
-
-	public void setContextAttribute(String key, Object value);
-
-	public void setContextAttribute(String key, Object value, boolean escape);
-
-	public void setContextAttributes(Object... values);
-
-	public void setContextUserPrefix(String contextUserPrefix);
-
-	public void setEntryTitle(String entryTitle);
-
-	public void setEntryURL(String entryURL);
-
-	public void setFrom(String fromAddress, String fromName);
-
-	public void setGroupId(long groupId);
-
-	public void setHtmlFormat(boolean htmlFormat);
-
-	public void setInReplyTo(String inReplyTo);
-
-	public void setLocalizedBodyMap(Map<Locale, String> localizedBodyMap);
-
-	public void setLocalizedSubjectMap(Map<Locale, String> localizedSubjectMap);
-
-	public void setMailId(String popPortletPrefix, Object... ids);
-
-	public void setNotificationClassNameId(long notificationClassNameId);
-
-	public void setNotificationType(int notificationType);
-
-	public void setPortletId(String portletId);
-
-	public void setReplyToAddress(String replyToAddress);
-
-	public void setScopeGroupId(long scopeGroupId);
-
-	public void setServiceContext(ServiceContext serviceContext);
-
-	public void setSMTPAccount(SMTPAccount smtpAccount);
-
-	public void setSubject(String subject);
-
-	public void setUniqueMailId(boolean uniqueMailId);
-
-	public void setUserId(long userId);
+	public void notify(BaseModel baseModel);
 
 }
