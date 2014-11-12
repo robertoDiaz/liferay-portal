89933b1b2b99bd68b67ef1b58363d866e04ae1eb
Roberto Díaz <roberto.diaz@liferay.com>
2014-11-12 12:41:00 +0100

    LPS-51280 Do not use SubscriptionSender logic inside the *Permission.java classes. Move that logic to SSender.

--- modules/apps/bookmarks/bookmarks-service/src/com/liferay/bookmarks/service/permission/BookmarksPermission.java
+++ modules/apps/bookmarks/bookmarks-service/src/com/liferay/bookmarks/service/permission/BookmarksPermission.java
@@ -14,7 +14,6 @@
 
 package com.liferay.bookmarks.service.permission;
 
-import com.liferay.bookmarks.constants.BookmarksConstants;
 import com.liferay.bookmarks.constants.BookmarksPortletKeys;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.staging.permission.StagingPermissionUtil;
