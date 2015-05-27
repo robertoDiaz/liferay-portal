c8ce026190c6cf05e9be78874b7e2ea17892c100
Roberto Díaz <roberto.diaz@liferay.com>
2015-05-28 01:02:07 +0200

    LPS-55922 Create a bridge to obtain the item selector url

---
+++ modules/apps/item-selector/item-selector-bridge-api/src/ItemSelectorBridge.java
@@ -0,0 +1,42 @@
+
+
+import com.liferay.document.library.item.selector.web.DLItemSelectorCriterion;
+import com.liferay.item.selector.ItemSelector;
+import com.liferay.portal.kernel.item.selector.PortalItemSelector;
+import com.liferay.portal.util.PropsValues;
+import com.liferay.portlet.documentlibrary.model.DLFolderConstants;
+
+import javax.portlet.PortletRequest;
+import javax.portlet.PortletURL;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Roberto Díaz
+ */
+@Component
+public class ItemSelectorBridge implements PortalItemSelector {
+
+	@Override
+	public PortletURL getItemSelectorURL(
+		PortletRequest portletRequest, String itemSelectedCallback,
+		long groupId, Class<?>... desiredReturnTypes) {
+
+		DLItemSelectorCriterion dlItemSelectorCriterion =
+			new DLItemSelectorCriterion(
+				DLFolderConstants.DEFAULT_PARENT_FOLDER_ID, groupId, "image",
+				PropsValues.DL_FILE_ENTRY_PREVIEW_IMAGE_MIME_TYPES, false);
+
+		return _itemSelector.getItemSelectorURL(
+			portletRequest, itemSelectedCallback, dlItemSelectorCriterion);
+	}
+
+	@Reference
+	public void setItemSelector(ItemSelector itemSelector) {
+		_itemSelector = itemSelector;
+	}
+
+	private ItemSelector _itemSelector;
+
+}
