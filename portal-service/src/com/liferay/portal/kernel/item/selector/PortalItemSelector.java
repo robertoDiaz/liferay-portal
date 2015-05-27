fd3f65d877e8e0449388c0486e40b464026ca5bd
Roberto Díaz <roberto.diaz@liferay.com>
2015-05-28 01:01:27 +0200

    LPS-55922 Create an interfaz to use the item selector API in portal

---
+++ portal-service/src/com/liferay/portal/kernel/item/selector/PortalItemSelector.java
@@ -0,0 +1,33 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.kernel.item.selector;
+
+import javax.portlet.PortletRequest;
+import javax.portlet.PortletURL;
+
+/**
+ *
+ * This class is for internal use. Please use {@link
+ * com.liferay.item.selector.ItemSelector}
+ *
+ * @author Roberto Díaz
+ */
+public interface PortalItemSelector {
+
+	public PortletURL getItemSelectorURL(
+		PortletRequest portletRequest, String itemSelectedCallback,
+		long groupId, Class<?>... desiredReturnTypes);
+
+}
