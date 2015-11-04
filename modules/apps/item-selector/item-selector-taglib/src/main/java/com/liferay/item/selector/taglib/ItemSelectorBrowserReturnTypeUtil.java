0f74237f294f0a39c14ed31018ca3a09984aceb9
Carlos Lancha <carlos.lancha@liferay.com>
2015-11-04 12:19:12 +0100

    LPS-59436 Update ItemSelectorBrowserReturnTypeUtil logic

--- modules/apps/item-selector/item-selector-taglib/src/main/java/com/liferay/item/selector/taglib/ItemSelectorBrowserReturnTypeUtil.java
+++ modules/apps/item-selector/item-selector-taglib/src/main/java/com/liferay/item/selector/taglib/ItemSelectorBrowserReturnTypeUtil.java
@@ -19,14 +19,21 @@ import com.liferay.item.selector.criteria.Base64ItemSelectorReturnType;
 import com.liferay.item.selector.criteria.FileEntryItemSelectorReturnType;
 import com.liferay.item.selector.criteria.URLItemSelectorReturnType;
 import com.liferay.item.selector.criteria.UploadableFileReturnType;
+import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.json.JSONFactoryUtil;
 import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.repository.model.FileEntry;
+import com.liferay.portal.kernel.repository.model.FileVersion;
 import com.liferay.portal.kernel.util.ClassUtil;
 import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.theme.ThemeDisplay;
+import com.liferay.portal.util.PropsValues;
+import com.liferay.portlet.documentlibrary.util.AudioProcessorUtil;
 import com.liferay.portlet.documentlibrary.util.DLUtil;
+import com.liferay.portlet.documentlibrary.util.ImageProcessorUtil;
+import com.liferay.portlet.documentlibrary.util.VideoProcessorUtil;
 
 import java.util.Iterator;
 import java.util.List;
@@ -80,11 +87,71 @@ public class ItemSelectorBrowserReturnTypeUtil
 		fileEntryJSONObject.put("fileEntryId", fileEntry.getFileEntryId());
 		fileEntryJSONObject.put("groupId", fileEntry.getGroupId());
 		fileEntryJSONObject.put("title", fileEntry.getTitle());
-		fileEntryJSONObject.put(
-			"url", DLUtil.getImagePreviewURL(fileEntry, themeDisplay));
+
+		String[] previewFileURLs = getURLs(fileEntry, themeDisplay);
+
+		fileEntryJSONObject.put("url", previewFileURLs[0]);
 		fileEntryJSONObject.put("uuid", fileEntry.getUuid());
 
 		return fileEntryJSONObject.toString();
+	}
+
+	protected static String[] getURLs(
+			FileEntry fileEntry, ThemeDisplay themeDisplay)
+		throws PortalException {
+
+		FileVersion fileVersion = fileEntry.getLatestFileVersion(true);
+
+		boolean hasAudio = AudioProcessorUtil.hasAudio(fileVersion);
+		boolean hasImages = ImageProcessorUtil.hasImages(fileVersion);
+		boolean hasVideo = VideoProcessorUtil.hasVideo(fileVersion);
+
+		String[] previewFileURLs = null;
+		String videoThumbnailURL = null;
+
+		String previewQueryString = null;
+
+		if (hasAudio) {
+			previewQueryString = "&audioPreview=1";
+		}
+		else if (hasImages) {
+			previewQueryString = "&imagePreview=1";
+		}
+
+		else if (hasVideo) {
+			previewQueryString = "&videoPreview=1";
+
+			videoThumbnailURL = DLUtil.getPreviewURL(
+				fileEntry, fileVersion, themeDisplay, "&videoThumbnail=1");
+		}
+
+		if (Validator.isNotNull(previewQueryString)) {
+			if (hasAudio) {
+				previewFileURLs = new String[PropsValues.DL_FILE_ENTRY_PREVIEW_AUDIO_CONTAINERS.length];
+
+				for (int i = 0; i < PropsValues.DL_FILE_ENTRY_PREVIEW_AUDIO_CONTAINERS.length; i++) {
+					previewFileURLs[i] = DLUtil.getPreviewURL(fileEntry, fileVersion, themeDisplay, previewQueryString + "&type=" + PropsValues.DL_FILE_ENTRY_PREVIEW_AUDIO_CONTAINERS[i]);
+				}
+			} else if (hasVideo) {
+				if (PropsValues.DL_FILE_ENTRY_PREVIEW_VIDEO_CONTAINERS.length > 0) {
+					previewFileURLs = new String[PropsValues.DL_FILE_ENTRY_PREVIEW_VIDEO_CONTAINERS.length];
+
+					for (int i = 0; i < PropsValues.DL_FILE_ENTRY_PREVIEW_VIDEO_CONTAINERS.length; i++) {
+						previewFileURLs[i] = DLUtil.getPreviewURL(fileEntry, fileVersion, themeDisplay, previewQueryString + "&type=" + PropsValues.DL_FILE_ENTRY_PREVIEW_VIDEO_CONTAINERS[i]);
+					}
+				} else {
+					previewFileURLs = new String[1];
+
+					previewFileURLs[0] = videoThumbnailURL;
+				}
+			} else {
+				previewFileURLs = new String[1];
+
+				previewFileURLs[0] = DLUtil.getPreviewURL(fileEntry, fileVersion, themeDisplay, previewQueryString);
+			}
+		}
+
+		return previewFileURLs;
 	}
 
 	protected static ItemSelectorReturnType
@@ -112,7 +179,9 @@ public class ItemSelectorBrowserReturnTypeUtil
 			FileEntry fileEntry, ThemeDisplay themeDisplay)
 		throws Exception {
 
-		return DLUtil.getImagePreviewURL(fileEntry, themeDisplay);
+		String[] previewFileURLs = getURLs(fileEntry, themeDisplay);
+
+		return previewFileURLs[0];
 	}
 
 	private static final List<String> _draggableFileReturnTypeNames =
