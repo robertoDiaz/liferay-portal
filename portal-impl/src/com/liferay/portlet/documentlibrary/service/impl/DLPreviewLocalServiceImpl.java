78e0be01d1969df681715c6c467564277e404812
Lianne Louie <lianne.louie@liferay.com>
2018-11-30 18:21:51 -0800

    LPS-86455 - add implementation for DLPreview

--- portal-impl/src/com/liferay/portlet/documentlibrary/service/impl/DLPreviewLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/documentlibrary/service/impl/DLPreviewLocalServiceImpl.java
@@ -14,7 +14,16 @@
 
 package com.liferay.portlet.documentlibrary.service.impl;
 
+import com.liferay.document.library.kernel.model.DLFileEntry;
+import com.liferay.document.library.kernel.model.DLPreview;
+import com.liferay.document.library.kernel.model.DLPreviewConstants;
+import com.liferay.document.library.kernel.service.DLFileEntryLocalServiceUtil;
+import com.liferay.document.library.kernel.service.persistence.DLPreviewUtil;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.repository.model.FileVersion;
 import com.liferay.portlet.documentlibrary.service.base.DLPreviewLocalServiceBaseImpl;
+
+import java.util.List;
 
 /**
  * The implementation of the document library preview local service.
@@ -27,6 +36,7 @@ import com.liferay.portlet.documentlibrary.service.base.DLPreviewLocalServiceBas
  * </p>
  *
  * @author Brian Wing Shun Chan
+ * @author Lianne Louie
  * @see DLPreviewLocalServiceBaseImpl
  * @see com.liferay.document.library.kernel.service.DLPreviewLocalServiceUtil
  */
@@ -37,5 +47,87 @@ public class DLPreviewLocalServiceImpl extends DLPreviewLocalServiceBaseImpl {
 	 *
 	 * Never reference this class directly. Always use {@link com.liferay.document.library.kernel.service.DLPreviewLocalServiceUtil} to access the document library preview local service.
 	 */
+	public DLPreview addDLPreview(long fileEntryId, long fileVersionId)
+		throws PortalException {
+
+		return addDLPreview(
+			fileEntryId, fileVersionId, DLPreviewConstants.STATUS_NOT_CREATED);
+	}
+
+	public DLPreview addDLPreview(
+			long fileEntryId, long fileVersionId, String status)
+		throws PortalException {
+
+		long fileEncryptionId = counterLocalService.increment();
+
+		DLPreview dlPreview = dlPreviewPersistence.create(fileEncryptionId);
+
+		dlPreview.setFileEntryId(fileEntryId);
+		dlPreview.setFileVersionId(fileVersionId);
+		dlPreview.setStatus(status);
+
+		DLFileEntry dlFileEntry = DLFileEntryLocalServiceUtil.getDLFileEntry(
+			fileEntryId);
+
+		long groupId = dlFileEntry.getGroupId();
+
+		dlPreview.setGroupId(groupId);
+
+		dlPreviewPersistence.update(dlPreview);
+
+		return dlPreview;
+	}
+
+	public DLPreview fetchDLPreview(FileVersion fileVersion) {
+		long fileVersionId = fileVersion.getFileVersionId();
+
+		List<DLPreview> dlPreviewList = DLPreviewUtil.findByFileVersionId(
+			fileVersionId);
+
+		if (dlPreviewList.isEmpty()) {
+			return null;
+		}
+
+		return dlPreviewList.get(0);
+	}
+
+	public String getDLPreviewStatus(FileVersion fileVersion) {
+		long fileVersionId = fileVersion.getFileVersionId();
+
+		List<DLPreview> dlPreviewList = DLPreviewUtil.findByFileVersionId(
+			fileVersionId);
+
+		if (dlPreviewList.isEmpty()) {
+			return null;
+		}
+
+		DLPreview dlPreview = dlPreviewList.get(0);
+
+		return dlPreview.getStatus();
+	}
+
+	public String getDLPreviewStatus(long fileEntryId) throws PortalException {
+		DLFileEntry fileEntry = DLFileEntryLocalServiceUtil.getFileEntry(
+			fileEntryId);
+
+		FileVersion fileVersion = (FileVersion)fileEntry.getLatestFileVersion(
+			true);
+
+		return getDLPreviewStatus(fileVersion);
+	}
+
+	public DLPreview updateDLPreview(
+		long dlPreviewId, long fileEntryId, long fileVersionId, String status) {
+
+		DLPreview dlPreview = DLPreviewUtil.fetchByPrimaryKey(dlPreviewId);
+
+		dlPreview.setFileEntryId(fileEntryId);
+		dlPreview.setFileVersionId(fileVersionId);
+		dlPreview.setStatus(status);
+
+		dlPreviewPersistence.update(dlPreview);
+
+		return dlPreview;
+	}
 
 }
