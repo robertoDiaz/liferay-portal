0639855bbae9898a48bc4336bfb4584e9dfc43c6
Roberto Díaz <roberto.diaz@liferay.com>
2019-01-30 11:35:04 +0100

    LPS-89349 Create new action to handle the upload

---
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-web/src/main/java/com/liferay/dynamic/data/mapping/form/web/internal/upload/FormsUploadResponseHandler.java
@@ -0,0 +1,57 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.dynamic.data.mapping.form.web.internal.upload;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.repository.model.FileEntry;
+import com.liferay.portal.kernel.upload.UploadPortletRequest;
+import com.liferay.upload.UploadResponseHandler;
+
+import javax.portlet.PortletRequest;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Roberto Díaz
+ */
+@Component(service = FormsUploadResponseHandler.class)
+public class FormsUploadResponseHandler implements UploadResponseHandler {
+
+	@Override
+	public JSONObject onFailure(
+			PortletRequest portletRequest, PortalException pe)
+		throws PortalException {
+
+		JSONObject jsonObject = _defaultUploadResponseHandler.onFailure(
+			portletRequest, pe);
+
+		return jsonObject;
+	}
+
+	@Override
+	public JSONObject onSuccess(
+			UploadPortletRequest uploadPortletRequest, FileEntry fileEntry)
+		throws PortalException {
+
+		return _defaultUploadResponseHandler.onSuccess(
+			uploadPortletRequest, fileEntry);
+	}
+
+	@Reference(target = "(upload.response.handler.system.default=true)")
+	private UploadResponseHandler _defaultUploadResponseHandler;
+
+}
