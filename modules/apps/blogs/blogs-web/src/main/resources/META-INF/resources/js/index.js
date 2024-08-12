3856270b5e2e083f506bd922e2cce03000c9942c
Jorge González <jorge.gonzalez@liferay.com>
2024-08-12 13:26:57 +0200

    LPD-26659 Remove old component

--- modules/apps/blogs/blogs-web/src/main/resources/META-INF/resources/js/index.js
+++ modules/apps/blogs/blogs-web/src/main/resources/META-INF/resources/js/index.js
@@ -3,7 +3,6 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-export {default as AssetCategoriesFriendlyUrlSelector} from './blogs/AssetCategoriesFriendlyUrlSelector';
 export {default as AssetCategoriesSelectionBox} from './blogs/AssetCategoriesSelectionBox';
 export {default as blogs} from './blogs/blogs';
 
