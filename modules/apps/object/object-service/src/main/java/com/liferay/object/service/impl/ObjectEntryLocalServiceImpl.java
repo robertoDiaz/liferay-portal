de91324e6c81b90cb1ccc5b698050675651dc149
Brian Chan <brian.chan@liferay.com>
2024-08-23 14:52:09 -0300

    LPD-25633 SF

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
@@ -3500,6 +3500,14 @@ public class ObjectEntryLocalServiceImpl
 			return;
 		}
 
+		List<String> columnNames = new ArrayList<>();
+
+		columnNames.add(
+			dynamicObjectDefinitionLocalizationTable.getForeignKeyColumnName());
+		columnNames.add("languageId");
+
+		int count = 2;
+
 		StringBundler sb = new StringBundler();
 
 		sb.append("insert into ");
@@ -3508,14 +3516,6 @@ public class ObjectEntryLocalServiceImpl
 		sb.append(
 			dynamicObjectDefinitionLocalizationTable.getForeignKeyColumnName());
 		sb.append(", languageId");
-
-		List<String> columnNames = new ArrayList<>();
-
-		columnNames.add(
-			dynamicObjectDefinitionLocalizationTable.getForeignKeyColumnName());
-		columnNames.add("languageId");
-
-		int count = 2;
 
 		for (ObjectField objectField : objectFields) {
 			if (objectField.isRequired() &&
