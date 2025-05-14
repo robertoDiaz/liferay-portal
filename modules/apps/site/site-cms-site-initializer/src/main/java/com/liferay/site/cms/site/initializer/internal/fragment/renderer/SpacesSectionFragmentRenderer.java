424b19b2fba58fc1f0cda30ad4c7d0e1886a8fb8
Roberto Díaz <roberto.diaz@liferay.com>
2025-05-14 12:03:53 +0200

    LPD-55576 Use the new page

--- modules/apps/site/site-cms-site-initializer/src/main/java/com/liferay/site/cms/site/initializer/internal/fragment/renderer/SpacesSectionFragmentRenderer.java
+++ modules/apps/site/site-cms-site-initializer/src/main/java/com/liferay/site/cms/site/initializer/internal/fragment/renderer/SpacesSectionFragmentRenderer.java
@@ -130,6 +130,11 @@ public class SpacesSectionFragmentRenderer extends BaseSectionFragmentRenderer {
 				).put(
 					"assetLibrariesCount", assetLibrariesCount
 				).put(
+					"newSpaceURL",
+					StringBundler.concat(
+						themeDisplay.getPathFriendlyURLPublic(),
+						GroupConstants.CMS_FRIENDLY_URL, "/new-space")
+				).put(
 					"showAddButton",
 					_portletResourcePermission.contains(
 						themeDisplay.getPermissionChecker(),
