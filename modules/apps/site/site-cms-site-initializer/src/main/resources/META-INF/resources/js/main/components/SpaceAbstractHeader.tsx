1659fa250322da55e498a1c542ea2f1d69704d46
Bruno Carvalho <bruno.carvalho@liferay.com>
2025-07-04 18:21:29 -0300

    LPD-57852 Open space members modal on space abstract header

--- modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/main/components/SpaceAbstractHeader.tsx
+++ modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/main/components/SpaceAbstractHeader.tsx
@@ -3,41 +3,71 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
+import ClayButton from '@clayui/button';
 import ClayLink from '@clayui/link';
 import {openModal} from 'frontend-js-components-web';
 import React from 'react';
 
+import SpaceMembersModalContent from '../spaces/SpaceMembersModal';
+
+enum SpaceAbstractHeaderActions {
+	OPEN_MEMBERS_MODAL = 'open-members-modal',
+}
+
+type ModalPropsType = {
+	action: SpaceAbstractHeaderActions;
+	assetLibraryCreatorUserId: string;
+	assetLibraryId: string;
+};
+
 interface SpaceAbstractHeaderProps {
 	label: string;
+	modalProps?: ModalPropsType;
 	title: string;
 	url: string;
 }
 
 export default function SpaceAbstractHeader({
 	label,
+	modalProps,
 	title,
 	url,
 }: SpaceAbstractHeaderProps) {
-	const openAbstractModal = () => {
+	const openMembersModal = (props: ModalPropsType) => {
 		openModal({
-			bodyHTML: `<p>${label}</p>`,
 			center: true,
-			containerProps: {},
-			size: 'lg',
+			contentComponent: () =>
+				SpaceMembersModalContent({
+					assetLibraryCreatorUserId: props.assetLibraryCreatorUserId,
+					assetLibraryId: props.assetLibraryId,
+				}),
+			size: 'md',
 			title,
 		});
+	};
+
+	const getActionCallback = () => {
+		if (
+			modalProps?.action === SpaceAbstractHeaderActions.OPEN_MEMBERS_MODAL
+		) {
+			return openMembersModal(modalProps);
+		}
 	};
 
 	return (
 		<div className="align-items-center d-flex justify-content-between">
 			<h2 className="font-weight-semi-bold m-0 text-4">{title}</h2>
 
-			{url ? (
-				<ClayLink href={url}>{label}</ClayLink>
-			) : (
-				<ClayLink href="#" onClick={openAbstractModal}>
+			{modalProps ? (
+				<ClayButton
+					displayType="link"
+					onClick={getActionCallback}
+					size="sm"
+				>
 					{label}
-				</ClayLink>
+				</ClayButton>
+			) : (
+				<ClayLink href={url}>{label}</ClayLink>
 			)}
 		</div>
 	);
